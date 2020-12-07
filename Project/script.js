7ec21ed86ff899708d3f7711a4305c80eb980f65
yiweihe13 <yhe170@syr.edu>
2020-12-06 23:12:35 -0500

    updated

--- Project/script.js
+++ Project/script.js
@@ -1,10 +1,11 @@
+$(document).ready(function() {
+  setTimeout(function(){
+	  setTimeout(function(){$(".introo").remove();}, 500);
+	  $(".introo").addClass("opc");
+	
+  },1000);
+});
+
 function revealMessage(){
 	document.getElementById("hiddenMessage").style.display ='block';
 }
-
-const tl = gsap.timeline({ defaults: { ease: ".intoo"}});
-
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".introo", { y: "-100%", duration: 1 }, "-=1");
-// JavaScript Document
